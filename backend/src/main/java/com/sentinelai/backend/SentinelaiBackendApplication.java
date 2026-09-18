1c72ef2840f9cab04f52fc982eadee3c24a70628
Anushka <23eg105b28@anurag.edu.in>
2026-09-18 17:19:13 +0530

    chore: initialize SentinelAI backend

---
+++ backend/src/main/java/com/sentinelai/backend/SentinelaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sentinelai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelaiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sentinelai/backend/SentinelaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentinelai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
